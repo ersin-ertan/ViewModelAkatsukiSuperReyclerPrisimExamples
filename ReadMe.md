f15cd33499e4997dc59adb69cfc9b363e79fdbad
ersin-ertan <ersin_the_ertan@hotmail.com>
2015-08-05 17:53:50 -0400

    tried akatsuki native example from the site, did not work, wait till the artifact comes out
    updated readme

---
+++ ReadMe.md
@@ -0,0 +1,47 @@
+###Developers
+ - [Mark Allison][0] StylingAndroid
+ - [inloop][1] Inloop
+ - [Anton Malinskiy][2] Malinskiy
+ - [Tom Lin][3] tom91136
+
+
+####StylingAndroid Libraries
+- [Prism][4]
+
+
+####Inloop Libraries
+- [AndroidViewModel][5]
+
+####Malinskiy Libraries
+- [SuperRecyclerView][6]
+
+####Tom91136 Libraries
+- [Akatsuki][7]
+
+
+####Worked Examples
+
+######[Prism][8]: A dynamic colouring library for Android https://blog.stylingandroid.com
+
+######[AndroidViewModel][9]: Separating data and state handling from Fragments or Activities without lots of boilerplate-code.
+
+######[SuperRecyclerView][10]: Pumped up RecyclerView
+
+######[Akatsuki][11]: Akatsuki is an Android library that handles state restoration via annotations.
+
+
+[0]: https://github.com/StylingAndroid
+[1]: https://github.com/inloop
+[2]: https://github.com/Malinskiy
+[3]: https://github.com/tom91136
+
+
+[4]: https://github.com/StylingAndroid/prism
+[5]: https://github.com/inloop/AndroidViewModel
+[6]: https://github.com/Malinskiy/SuperRecyclerView
+[7]: https://github.com/tom9136/Akatsuki
+
+[8]: https://github.com/ersin-ertan/ViewModelAkatsukiSuperReyclerPrisimExamples/tree/master/annotatedadapter/src/main/java/com/nullcognition/prisim
+[9]: https://github.com/ersin-ertan/ViewModelAkatsukiSuperReyclerPrisimExamples/tree/master/parcelableplease/src/main/java/com/nullcognition/androidviewmodel
+[10]: https://github.com/ersin-ertan/ViewModelAkatsukiSuperReyclerPrisimExamples/tree/master/adapterdelegates/src/main/java/com/nullcognition/superrecyclerview
+[11]: https://github.com/ersin-ertan/ViewModelAkatsukiSuperReyclerPrisimExamples/tree/master/intentbuilder/src/main/java/com/nullcognition/akatsuki
--- akatsuki/src/main/java/com/nullcognition/akatsuki/MainActivity.java
+++ akatsuki/src/main/java/com/nullcognition/akatsuki/MainActivity.java
@@ -1,38 +1,63 @@
 package com.nullcognition.akatsuki;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.sora.util.akatsuki.Akatsuki;
+import com.sora.util.akatsuki.Retained;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity{
+
+
+	private static final String MY_KEY = "myString";
+	@Retained
+	String myString;
+	@Retained
+	Float myFloat = 3F;
+
+	@Bind(R.id.persisted)
+	EditText persisted;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ButterKnife.bind(this);
+
+		Akatsuki.restore(this, savedInstanceState);
+
+
+		persisted.setText(myString);
+		persisted.addTextChangedListener(new TextWatcher(){
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after){
+
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count){
+
+			}
+
+			@Override
+			public void afterTextChanged(Editable s){
+				myString = persisted.getText().toString();
+			}
+		});
 	}
 
-
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu){
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item){
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if(id == R.id.action_settings){
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
+	protected void onSaveInstanceState(Bundle outState){
+		super.onSaveInstanceState(outState);
+		outState.putString(MY_KEY, myString);
+		Akatsuki.save(this, outState);
 	}
 }
